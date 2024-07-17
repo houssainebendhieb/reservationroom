6abbde1f142ff104b32e01a058533e72d04f012e
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2024-07-17 09:40:14 +0100

    initial project with initial firebase setup

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main()async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MaterialApp(
+    home: Text('hello world '),
+  ));
+}
